53812c2adcf2f12874031197186f92a4b6af0d1c
Brian Ryckbost <bryckbost@gmail.com>
2012-06-01 15:23:44 -0400

    Commit all the rails files

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.5'
+gem 'jquery-rails'
+gem 'figaro'
+gem 'bson_ext'
+gem 'mongo_mapper'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Garner::Application.config.secret_token = 'a4babcc092868178a8909a26c252323154b5133e5f2ee6ab460c996423126754873efc5f362f1e368f0d3a6caeeddc1010c8c91c9a7aa1e9fe8812312bd7fa95'
